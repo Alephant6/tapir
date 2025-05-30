ccbe1ec016460f4b5d270670f20b319347c78d9d
sqiang <sqiang@buffalo.edu>
2025-05-29 22:48:41 -0600

    sudo

--- store/tools/send.sh
+++ store/tools/send.sh
@@ -3,7 +3,7 @@
 for host in node1 node2 node3 node4
 do
   echo "Deleting /local/tapir on $host..."
-  ssh "$host" "rm -rf /local/tapir"
+  ssh "$host" "sudo rm -rf /local/tapir"
   echo "Copying new tapir folder to $host..."
   scp -rq /local/tapir "$host":/local
 done
